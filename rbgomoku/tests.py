68e95154b67c47ef9e4aab347630aef05d5355d1
Willian de Souza <willianstosouza@gmail.com>
2016-04-15 20:34:25 -0300

    search victory by row and column

--- rbgomoku/tests.py
+++ rbgomoku/tests.py
@@ -1,46 +1,70 @@
 import unittest
-from view import Board
+from view import Board, Piece
+
+def print_test(s):
+    print("{0:#^50}".format(s))
 
 class BoardTests(unittest.TestCase):
     def setUp(self):
-        self.board = Board(3, 3)
+        self.board = Board(size=15, sequence_victory=5)
 
     def test_board_of_table(self):
-        expected_table = '   0  1  2 \n' \
-                         '0  .  .  . \n' \
-                         '1  .  .  . \n' \
-                         '2  .  .  . '
+        print_test(" starting test_board_of_table ")
+        expected_table = '   0  1  2  3  4  5  6  7  8  9  10 11 12 13 14\n' \
+                         '0  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '1  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '2  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '3  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '4  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '5  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '6  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '7  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '8  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '9  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '10 .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '11 .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '12 .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '13 .  .  .  .  .  .  .  .  .  .  .  .  .  .  . \n' \
+                         '14 .  .  .  .  .  .  .  .  .  .  .  .  .  .  . '
+        print(self.board)
+        print()
         self.assertEqual(expected_table, str(self.board))
 
     def test_get_piece(self):
+        print_test(" starting test_get_piece ")
         piece = self.board.get_piece(1,1)
-        self.assertEqual(Board.NONE, piece)
+        print()
+        self.assertEqual(Piece.NONE, piece)
 
     def test_play_piece(self):
+        print_test(" starting test_play_piece ")
         piece = self.board.get_piece(1, 1)
-        self.assertEqual(Board.NONE, piece)
-        self.board.play_piece(Board.BLACK, 1, 1)
+        self.assertEqual(Piece.NONE, piece)
+        self.board.play_piece(Piece.BLACK, 1, 1)
         piece = self.board.get_piece(1, 1)
-        self.assertEqual(Board.BLACK, piece)
-        self.board.play_piece(Board.WHITE, 0, 2)
+        self.assertEqual(Piece.BLACK, piece)
+        self.board.play_piece(Piece.WHITE, 0, 2)
         piece = self.board.get_piece(0, 2)
-        self.assertEqual(Board.WHITE, piece)
+        self.assertEqual(Piece.WHITE, piece)
         print(self.board)
+        print()
 
     def test_search_line(self):
-        line = self.board._table[1]
-        result = self.board.search_line(Board.BLACK, line)
+        print_test(" starting test_search_line ")
+        result = self.board.search_line(Piece.BLACK, row=1, col=6)
         self.assertFalse(result)
-        line = list('x' * 3)
-        result = self.board.search_line(Board.BLACK, line)
+        self.board._table[1, 5:10] = 'x'
+        result = self.board.search_line(Piece.BLACK, row=1, col=5)
+        print(self.board)
+        print()
         self.assertTrue(result)
 
     def test_search_column(self):
-        line = self.board._table[1]
-        result = self.board.search_line(Board.BLACK, line)
+        print_test(' starting test_search_column ')
+        result = self.board.search_column(Piece.BLACK, row=5, col=1)
         self.assertFalse(result)
-        line = list(('.' * 2) + ('x' * 3))
-        result = self.board.search_line(Board.BLACK, line)
+        self.board._table[5:10, 1] = 'x'
+        result = self.board.search_column(Piece.BLACK, row=9, col=1)
+        print(self.board)
+        print()
         self.assertTrue(result)
-
-
--- rbgomoku/view.py
+++ rbgomoku/view.py
@@ -1,11 +1,4 @@
-
-class Score:
-    ONE = 1
-    TWO = 20
-    THREE = 50
-    FOUR = 100
-    FIVE = 10000
-
+import numpy as np
 
 # class Gomoku:
 #     def __init__(self):
@@ -17,24 +10,48 @@ class Score:
 #         self._parent = None
 #         self._head = head
 #         self._subtree = {}
+class Score:
+    ONE = 1
+    TWO = 20
+    THREE = 50
+    FOUR = 100
+    FIVE = 10000
+
+class Piece:
+    """Piece representation
+    """
+    """Indicate the position in board does not played yet"""
+    NONE = '.'
+
+    """Indicate a BLACK piece for game"""
+    BLACK = 'x'
+
+    """Indicate a WHITE piece for game"""
+    WHITE = 'o'
 
 
 class Board:
-    NONE = '.'
-    BLACK = 'x'
-    WHITE = 'o'
+    """Board representation of the console game
+    """
+    def __init__(self, size, sequence_victory):
+        """Start the Board with empty positions
+        :param size: size of board sizeXsize format
+        :param sequence_victory: number of piece in sequence to see victory
+        """
+        self._table = np.chararray((size, size))
+        self._table[:] = Piece.NONE
+        self._table = self._table.astype(np.str)
+        self._winner = Piece.NONE
+        self._sequence_victory = sequence_victory
 
-    def __init__(self, size, sequency_victory):
-        self._table = []
-        self._winner = self.NONE
-        self._sequency_victory = sequency_victory
-
-        for i in range(size):
-            self._table.append(list(self.NONE * (size)))
 
     def __repr__(self):
-        formatter = ' '.join(['{' + str(i) + ':^2}' for i in range(len(self._table) + 1)])
-        range_table = range(len(self._table))
+        """Format the boarder
+        :return: the board in str format
+        """
+        table_copy = self._table.tolist()
+        formatter = ' '.join(['{' + str(i) + ':^2}' for i in range(len(table_copy) + 1)])
+        range_table = range(len(table_copy))
         idx_table = list(range_table)
 
         ## top index table ##
@@ -43,7 +60,7 @@ class Board:
         top_idx_f = formatter.format(*top_idx_table_str)
 
         ## left index table ##
-        left_idx_table = [[str(i)] + self._table[i] for i in range_table]
+        left_idx_table = [[str(i)] + table_copy[i] for i in range_table]
         left_idx_table_f = [formatter.format(*line) for line in left_idx_table]
 
         str_table = '\n'.join([top_idx_f] + left_idx_table_f)
@@ -54,20 +71,76 @@ class Board:
 
     def play_piece(self, piece, row, col):
         self._table[row][col] = piece
+        # return self.has_winner(piece, row, col)
+
+    def has_winner(self, piece, row, col):
+        if self.search_line(piece, row, col):
+            """Faz verificação por linha
+                se ocorrer uma vitória na linha atual
+                escreve a peça ganhadora e retorna verdadeiro
+            """
+            self._winner = piece
+            return True
+
+        if self.search_column(piece, row, col):
+            """Faz verificação por linha
+                se ocorrer uma vitória na linha atual
+                escreve a peça ganhadora e retorna verdadeiro
+            """
+            self._winner = piece
+            return True
+
+        # if self.search_line(piece, row, col):
+            # for j in
+            #
+            #
+            # for j in
 
     def victory_match(self, piece):
-        return piece * self._sequency_victory
+        """A generator to match victory
 
-    def search_line(self, piece, line):
+        :param piece: for check the victory sequence
+        :return: the sequence for victory
+        """
+        return piece * self._sequence_victory
+
+    def search_line(self, piece, row, col):
+        """search has victory in line
+
+                check in matrix row if has match value to victory
+                it get the previous five column position from current position played and
+                the next five column position from current and check if has victory
+
+        :param piece: current piece played
+        :param row: position in matrix
+        :param col: position in matrix
+        :return: if has a victory in current line, True
+                    otherwise is False
+        """
         match_str = self.victory_match(piece)
-        return match_str in ''.join(line)
+        start_col = 0 if (col - 5) < 0 else (col - 5)
+        size = len(self._table)
+        end_col = size if (col + 5 + 1) > size else (col + 5 + 1)
+        return match_str in ''.join(self._table[row][start_col:end_col])
 
+    def search_column(self, piece, row, col):
+        """search has victory in line
 
-    # def search_winner(self, piece):
-    #     size = len(self._table)
-    #     for i in range(size):
-    #         for j in
+                check in matrix column if has match value to victory
+                it get the previous five row position from current position played and
+                the next five row position from current and check if has victory
 
+        :param piece: current piece played
+        :param row: position in matrix
+        :param col: position in matrix
+        :return: if has a victory in current row, True
+                    otherwise is False
+        """
+        match_str = self.victory_match(piece)
+        start_row = 0 if (row - 5) < 0 else (row - 5)
+        size = len(self._table)
+        end_row = size if (row + 5 + 1) > size else (row + 5 + 1)
+        return match_str in ''.join(self._table[start_row:end_row, col])
 
 # class Human:
 #     human_piece = Board.BLACK
